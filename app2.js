0dd4212f477e668267c5c6cc59148f600a97e20d
Arun Sood <arsood@gmail.com>
2016-05-13 10:32:02 -0700

    Added temporary solutions

---
+++ app2.js
@@ -0,0 +1,38 @@
+//Require modules and models
+
+var express = require("express");
+var models = require("./models/index");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+
+//Set view engine
+
+app.set("view engine", "ejs");
+
+//Middleware
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(methodOverride("_method"));
+
+app.get("/", function(req, res) {
+    res.redirect(301, "/chirps");
+});
+
+//Get all chirps
+
+//Create new chirp
+
+//Get specific chirp
+
+//Edit a chirp
+
+//Delete a chirp
+
+app.listen(process.env.PORT || 3000);
